ee2d1fa7ae11f5614959532aca8278db2a764636
dojocat <dopersh+dojocat@gmail.com>
2019-03-01 00:26:38 +0900

    WIP

--- app/src/main/java/org/dongri/donpay/SettingActivity.java
+++ app/src/main/java/org/dongri/donpay/SettingActivity.java
@@ -1,25 +1,21 @@
 package org.dongri.donpay;
 
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class SettingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_setting);
 
         ListView listView = (ListView)findViewById(R.id.player_listview);
 
@@ -62,23 +58,5 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.settingButton:
-                Intent intent = new Intent(this, SettingActivity.class);
-//                EditText editText = (EditText) findViewById(R.id.editText);
-//                String message = editText.getText().toString();
-//                intent.putExtra(EXTRA_MESSAGE, message);
-                startActivity(intent);
-                break;
-        }
-        return true;
-    }
 }
